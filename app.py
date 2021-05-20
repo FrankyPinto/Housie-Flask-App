882ab9cd3d1c4d1c812897a9638ad0574be0b875
FrankyPinto <frankypin@gmail.com>
2021-05-21 01:02:11 +0530

    Login Added

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask import render_template, request, json
+from flask import render_template, request, json, session, redirect, url_for
 import random
 
 app = Flask(__name__)
+app.secret_key = "housie-flask-app"
 
 original_list = [1,2,3,4,5,6,7,8,9,10,
 11,12,13,14,15,16,17,18,19,20,
@@ -17,8 +18,44 @@ original_list = [1,2,3,4,5,6,7,8,9,10,
 recent_num = []
 numbers_called = []
 
-# Client Home URL
 @app.route("/")
+def home():
+    return render_template("loginpage.html")
+
+@app.route("/loginapi", methods=['POST'])
+def login_api():
+    if "user_client" in session:
+        return redirect(url_for("homepage"))
+    elif "user_admin" in session:
+        return redirect(url_for("adminhome"))
+    elif request.method == "POST":
+        uname = request.form['u_name']
+        upwd = request.form['u_paswd']
+        if uname=="client" and upwd=="client":
+            session["user_client"] = uname
+            return redirect(url_for("homepage"))
+        elif uname=="admin" and upwd=="admin":
+            session["user_admin"] = uname
+            return redirect(url_for("adminhome"))
+        else:
+            return render_template("errortemp.html")
+        print(uname,upwd)
+    else:
+        return render_template("errortemp.html")
+
+@app.route("/logout")
+def logout():
+    if "user_client" in session:
+        session.pop("user_client",None)
+        return redirect(url_for("home"))
+    elif "user_admin" in session:
+        session.pop("user_admin",None)
+        return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
+# Client Home URL
+@app.route("/clienthome")
 def homepage():
     return render_template("clienthome.html")
 
